a194d94b145ba5b44a83c3c8d43daf9be609160c
JAY-Winter <rubat0@naver.com>
2022-10-20 23:53:13 +0900

    chore: secret key file name 수정
    파일 빌드시 오류나서 secret.py > secret_key.py 로 수정

--- LLATROF/LLATROF/settings.py
+++ LLATROF/LLATROF/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-from secrets import SECRET_KEY as private_secret_key
+from secrets_key import SECRET_KEY as private_secret_key
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'articles',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
