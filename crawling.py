3e3d2d11326154cee2ed63366aa7931e0d998f7b
JAY-Winter <rubat0@naver.com>
2022-10-20 10:33:00 +0900

    feat: 페이지 로드 시 최하단부로 이동 기능 구현
    로드 직후 데이터 parsing 시, 해당 goods 의 img_url 이 누락됨
    동작 중 0.05 대기 시간을 줘 해당 높이의 데이터 pasring

--- crawling.py
+++ crawling.py
@@ -7,3 +7,15 @@ driver = webdriver.Chrome("/Users/heyon/Desktop/JAY/LLATROF/chromedriver")
 URL = 'https://www.musinsa.com/categories/item/003?d_cat_cd=003&brand=&list_kind=small&sort=pop_category&sub_sort=&page=1&display_cnt=90&group_sale=&exclusive_yn=&sale_goods=&timesale_yn=&ex_soldout=&kids=&color=&price1=&price2=&shoeSizeOption=&tags=&campaign_id=&includeKeywords=&measure=measure_5%5E110%5E120'
 driver.get(URL)
 
+def scroll_to_bottom():
+    before_location = driver.execute_script("return window.pageYOffset")
+    while True:
+        # 현재 위치 + 100으로 스크롤 이동   
+        driver.execute_script(f"window.scrollTo(0,{before_location + 100})")
+        time.sleep(0.05)
+        after_location = driver.execute_script("return window.pageYOffset")
+        # 더 이상 스크롤이 늘어나지 않으면 종료
+        if before_location == after_location:
+            break
+        else:
+            before_location = driver.execute_script("return window.pageYOffset")
