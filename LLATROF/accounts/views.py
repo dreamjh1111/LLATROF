74fd77507eb84678a7002f6b968cda58cd9d0718
JAY-Winter <rubat0@naver.com>
2022-10-24 13:25:31 +0900

    feat: 회원가입 기능 decorator 및 forms 추가

--- LLATROF/accounts/views.py
+++ LLATROF/accounts/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
+from django.views.decorators.http import require_http_methods
 from .forms import CustomUserCreationForm
 
-# Create your views here.
+@require_http_methods(['GET', 'POST'])
 def signup(request):
     if request.method == 'POST':
         form = CustomUserCreationForm(request.POST)
