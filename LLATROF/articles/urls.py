5c5c81056c823c0cd3fddac809ed64891f083ce2
JAY-Winter <rubat0@naver.com>
2022-10-26 16:51:46 +0900

    feat: 필터링- 카테고리 분류 기능 구현

--- LLATROF/articles/urls.py
+++ LLATROF/articles/urls.py
@@ -4,4 +4,6 @@ from . import views
 app_name = 'articles'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('category/', views.category, name='category'),
+    path('category/<category>/', views.category_goods, name='category_goods'),
 ]
--- LLATROF/articles/views.py
+++ LLATROF/articles/views.py
@@ -8,3 +8,26 @@ def index(request):
         'articles': articles,
     }
     return render(request, 'articles/index.html', context)
+
+def category(request):
+    categorise = Article.objects.values('goods_category').distinct()
+
+    category_list = []
+    for category in categorise:
+        category = category['goods_category']
+        category_list.append(category)
+
+    context = {
+        'cetegories': category_list,
+    }
+    
+    return render(request, 'articles/category.html', context)
+
+
+def category_goods(request, category):
+    goods_list = Article.objects.all().filter(goods_category=category)
+    
+    context = {
+        'goods_list': goods_list,
+    }
+    return render(request, 'articles/category.html', context)
