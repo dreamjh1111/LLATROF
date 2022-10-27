fa73f4a64e5538170193a7357eb65ef251b18336
jay <rubat0@naver.com>
2022-10-27 14:18:18 +0900

    style: Type Anotation 추가
    Django - Variable Routing 기본Type 은 String
    다만, 명시적으로 표기하기 위해 타입 표기

--- LLATROF/articles/urls.py
+++ LLATROF/articles/urls.py
@@ -5,7 +5,7 @@ app_name = 'articles'
 urlpatterns = [
     path('', views.index, name='index'),
     path('category/', views.category, name='category'),
-    path('category/<category>/', views.category_goods, name='category_goods'),
+    path('category/<str:category>/', views.category_goods, name='category_goods'),
     path('brand/', views.brand, name='brand'),
-    path('brand/<brand>/', views.brand_goods, name='category_brand'),
+    path('brand/<str:brand>/', views.brand_goods, name='category_brand'),
 ]
