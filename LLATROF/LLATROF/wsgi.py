fb4cbcdf02af20c20b93c48982d634a960cd3c92
JAY-Winter <rubat0@naver.com>
2022-11-01 22:58:19 +0900

    build: Docker 연동을 위한 Project Name 변경
    파일 내 위치한 LLATROF 변경 후 디렉토리명 변경 예정

--- LLATROF/LLATROF/wsgi.py
+++ LLATROF/LLATROF/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for LLATROF project.
+WSGI config for llatrof project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LLATROF.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'llatrof.settings')
 
 application = get_wsgi_application()
